aa63a4b328b2acb124ff66759184c6f873a27c86
chandana.yeranagu@msg-global.com <chandana.yeranagu@msg-global.com>
2026-06-04 10:38:57 +0000

    first commit

---
+++ zsrt_tcr/webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("srt.tcr.zsrttcr.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
